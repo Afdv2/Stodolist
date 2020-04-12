3f08adf063b8d16f00514345ed6541c9c353c2a2
Alexandr <tsfadeev@yandex.ru>
2020-04-13 00:14:43 +0300

    remove all unnecessary code

---
+++ stodo/Main/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  stodo
+//
+//  Created by Alexandr on 12.04.2020.
+//  Copyright © 2020 clean-code.club. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .red
+        window?.rootViewController = viewController
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
