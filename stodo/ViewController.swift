f923004377e034dee36eef470a7174f855057a84
Alexandr <tsfadeev@yandex.ru>
2020-04-12 20:22:06 +0300

    first commit

---
+++ stodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stodo
+//
+//  Created by Alexandr on 12.04.2020.
+//  Copyright © 2020 clean-code.club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
