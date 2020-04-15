96cebf13d6f022849533a248605a08d1473a3dea
Alexandr <tsfadeev@yandex.ru>
2020-04-16 00:00:56 +0300

    Корневой контроллер UINavigationController

--- stodo/Main/AppDelegate.swift
+++ stodo/Main/AppDelegate.swift
@@ -15,10 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        let viewController = ListsViewController()
-        viewController.view.backgroundColor = .red
-        window?.rootViewController = viewController
-        // Override point for customization after application launch.
+        let listsViewController = ListsConfigurator().configure(shouldMock: true)
+        let rootViewController = UINavigationController(rootViewController: listsViewController)
+        window?.rootViewController = rootViewController
         return true
     }
 }
